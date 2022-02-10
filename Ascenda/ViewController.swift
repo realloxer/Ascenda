7ecd4445db762628436f7ecad4ad0a1b304d47f0
LocNguyen <locnguyen05081991@gmail.com>
2022-02-10 13:56:25 +0700

    Init project

---
+++ Ascenda/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ascenda
+//
+//  Created by LocNguyen on 10/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
